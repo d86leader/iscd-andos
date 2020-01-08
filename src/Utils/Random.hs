b56b9bc819df55914d04e98916e087d9a47e450d
d86leader <d86leader@mail.com>
2020-01-08 22:32:22 +0700

    fix snd of mapRandom being bottom

--- src/Utils/Random.hs
+++ src/Utils/Random.hs
@@ -7,6 +7,10 @@ import System.Random                   (RandomGen, next)
 
 mapRandom :: RandomGen g => (a -> g -> (b, g)) -> [a] -> g -> ([b], g)
 mapRandom fun xs gen =
-    let gens = gen : map (snd . next) gens
-        vals = zipWith fun xs gens
-    in (map fst vals, last gens)
+    let infiniteGens = gen : map (snd . next) infiniteGens
+        vals = zipWith fun xs infiniteGens
+        gens = zipWith const infiniteGens vals
+        lastGen = if null gens
+                    then gen
+                    else last gens
+    in (map fst vals, lastGen)
